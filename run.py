d6545c09af3093e1d73a4d570aaee819619756c2
Muhammad Awais <awaisshahid965@gmail.com>
2024-10-29 00:16:24 +0500

    chore: remove redundant code

--- run.py
+++ run.py
@@ -1,8 +1,5 @@
-from dotenv import load_dotenv
+from dotenv import load_dotenv; load_dotenv()
 from app.config.app import App
-
-# Load environment variables
-load_dotenv()
 
 if __name__ == "__main__":
     App.init_server()
