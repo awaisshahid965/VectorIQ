95b6ef5b38874fd5be7787b38985cd72142d0606
Muhammad Awais <awaisshahid965@gmail.com>
2024-10-29 00:23:46 +0500

    Update main.py

--- app/main.py
+++ app/main.py
@@ -7,10 +7,4 @@ app = FastAPI()
 def index():
     return "app is running..."
 
-def create_app():
-
-    app.include_router(chat_router)
-
-    return app
-
-app = create_app()
+app.include_router(chat_router)
