bd02c4815b78c214ba3fdc199434d5b7050d2da7
Muhammad Awais <awaisshahid965@gmail.com>
2024-10-29 00:20:48 +0500

    Update main.py

--- app/main.py
+++ app/main.py
@@ -1,8 +1,13 @@
 from fastapi import FastAPI
 from app.controllers.chat import router as chat_router
 
+app = FastAPI()
+
+@app.get('/')
+def index():
+    return "app is running..."
+
 def create_app():
-    app = FastAPI()
 
     app.include_router(chat_router)
 
