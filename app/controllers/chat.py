7c3ed329831c8e891f625da4f9467a6b7f6b288b
Muhammad Awais <awaisshahid965@gmail.com>
2024-10-28 23:23:28 +0500

    fix: implement server sent events for streaming response

--- app/controllers/chat.py
+++ app/controllers/chat.py
@@ -1,11 +1,17 @@
-from fastapi import APIRouter, Request
-from fastapi.responses import StreamingResponse
-from langchain.chains import create_retrieval_chain
-from langchain_core.runnables import RunnablePassthrough
-from langchain_openai import ChatOpenAI
+from dotenv import load_dotenv
+
 from app.vector_stores.qdrant_vector_store import QdrantVectoreStore
-from langchain import hub
+
+from fastapi.responses import StreamingResponse
+from fastapi import APIRouter, status, HTTPException
+
+from langchain_openai import ChatOpenAI
+from langchain.chains import create_retrieval_chain
+from langchain_core.prompts import ChatPromptTemplate
 from langchain.chains.combine_documents import create_stuff_documents_chain
+
+# Load environment variables
+load_dotenv()
 
 router = APIRouter()
 
@@ -15,23 +21,40 @@ model = ChatOpenAI(
     temperature=0,
 )
 
-# Pulling prompt from hub
-retrieval_qa_chat_prompt = hub.pull("langchain-ai/retrieval-qa-chat")
+retrieval_qa_chat_prompt = ChatPromptTemplate.from_messages([
+  ("system", "Answer any user questions based solely on the context below. Format each of your responses in Markdown, and for each line break, use `<br>` at the end of the line. Ensure that all bullet points, sections, and paragraphs are properly separated with `<br>` for clearer formatting:\n<context>\n{context}\n</context>"),
+  ("placeholder", "{chat_history}"),
+  ("human", "{input}"),
+])
+
 combine_docs_chain = create_stuff_documents_chain(model, retrieval_qa_chat_prompt)
 
 rag_chain = create_retrieval_chain(qdrant_vector_store.get_retriever(), combine_docs_chain)
 
-async def answer_stream(query: str):
-    for chunk in rag_chain.stream({"input": query}):
-        if answer_chunk := chunk.get("answer"):
-            yield f"{answer_chunk}\n"
+def answer_stream(query: str):
+    try:
+        for chunk in rag_chain.stream({"input": query}):
+            if answer_chunk := chunk.get("answer"):
+                yield f"{answer_chunk}\n\n"
+    except Exception as e:
+        raise RuntimeError(f"Error while streaming: {str(e)}")
 
-@router.post("/query/")
-async def query_endpoint(request: Request):
-    # Extract query from the request body (assuming JSON payload)
-    body = await request.json()
-    query = body.get("query")
-    
-    # Return the streamed response
-    return StreamingResponse(answer_stream(query), media_type="text/plain")
+@router.get("/query/")
+def query_endpoint(query: str):
 
+    if not query:
+        raise HTTPException(status_code=400, detail="Query not provided")
+
+    try:
+        return StreamingResponse(
+            content=answer_stream(query),
+            media_type="text/event-stream",
+            headers={
+                "Cache-Control": "no-cache",
+                "Connection": "keep-alive",
+                "X-Accel-Buffering": "no",
+            },
+            status_code=status.HTTP_200_OK,
+        )
+    except RuntimeError as e:
+        raise HTTPException(status_code=500, detail=str(e))
